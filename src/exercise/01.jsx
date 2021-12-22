fb234fb01d9c18f8523c29fccd78df5128c6f9e7
akif <akifunal05@gmail.com>
2021-12-22 22:56:40 +0300

    userReducer: simple counter developed

--- src/exercise/01.jsx
+++ src/exercise/01.jsx
@@ -1,18 +1,32 @@
 // useReducer: simple Counter
 // http://localhost:3000/isolated/exercise/01.js
+// @ts-nocheck
+import {useReducer} from 'react'
 
-import * as React from 'react'
+const enums = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+}
+
+function countReducer(state, action) {
+  const {type, payload} = action
+
+  switch (type) {
+    case enums.INCREMENT:
+      return {...state, count: state.count + payload}
+
+    default: {
+      throw new Error(`Unsupported action type: ${type}`)
+    }
+  }
+}
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+  const [state, dispatch] = useReducer(countReducer, {count: initialCount})
+  const {count} = state
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+  const increment = () => dispatch({type: enums.INCREMENT, payload: step})
+
   return <button onClick={increment}>{count}</button>
 }
 
